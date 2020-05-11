693578acb22a05a7289202626665a70e6efb0b4b
john <john4pius@gmail.com>
2020-05-11 05:48:55 +0100

    Code Format
    fix PEP8 requirements.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,9 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format(
+            'localhost:5432', self.database_name
+        )
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,25 +26,26 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-            
+
         self.demo = {
             "question": "Whats my name?",
             "answer": "John",
             "difficulty": 1,
             "category": "2",
-        }    
-    
+        }
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
     """
     TODO
-    Write at least one test for each test for successful operation and for expected errors.
+    Write at least one test for each test for successful operation and for
+    expected errors.
     """
-    
+
     def test_get_questions(self):
-    
+
         response = self.client().get("/questions")
         data = json.loads(response.data)
 
